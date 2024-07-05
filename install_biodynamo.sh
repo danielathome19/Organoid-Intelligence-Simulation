c50933b2894f4bea76041a534aacf878a7d668e9
Daniel J. Szelogowski <dszelogowski@gmail.com>
2024-07-05 04:46:07 -0500

    Initial commit

--- install_biodynamo.sh
+++ install_biodynamo.sh
@@ -1,2 +1,58 @@
 #!/bin/bash
-curl https://biodynamo.github.io/install | bash
+# curl https://biodynamo.github.io/install | bash
+
+set -e
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update && apt-get install -y \
+    clang \
+    freeglut3-dev \
+    wget \
+    clang-format \
+    g++ \
+    zlib1g-dev \
+    clang-tidy \
+    gcc \
+    libbz2-dev \
+    doxygen \
+    git \
+    libffi-dev \
+    graphviz \
+    libblas-dev \
+    liblzma-dev \
+    libxml2-dev \
+    libbz2-dev \
+    libreadline-dev \
+    llvm-7 \
+    liblapack-dev \
+    libnuma-dev \
+    libssl-dev \
+    llvm-7-dev \
+    libomp5 \
+    python-openssl \
+    valgrind \
+    libomp-dev \
+    tk-dev \
+    libgsl-dev \
+    libopenmpi-dev \
+    xz-utils \
+    cmake \
+    libpthread-stubs0-dev \
+    curl \
+    make \
+    python3-setuptools \
+    python3-wheel \
+    python3-pip
+
+git clone https://github.com/BioDynaMo/biodynamo.git /biodynamo
+cd /biodynamo
+git checkout v1.04-patches
+
+mkdir build
+cd build
+cmake ..
+make -j4
+make install
+
+cd /
+rm -rf /biodynamo
