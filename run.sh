c264aa80414c2edb887f0d644870b2489dcce20e
Daniel J. Szelogowski <dszelogowski@gmail.com>
2024-07-05 23:51:34 -0500

    Initial commit

--- run.sh
+++ run.sh
@@ -7,14 +7,19 @@ python3 mlp_model.py
 
 
 # Step 2
-echo "Building the BioDynaMo simulation..."
-mkdir -p build
+echo "Preparing the build directory..."
+rm -rf build
+mkdir build
 cd build
+
+
+# Step 3
+echo "Building the BioDynaMo simulation..."
 cmake ..
 make
 
 
-# Step 3
+# Step 4
 echo "Running the BioDynaMo simulation..."
 ./biodynamo_simulation
 
